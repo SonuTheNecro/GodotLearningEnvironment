ce89db98d16e15727c012f00fec1624e15c98c92
Sonu <bhaarat.mohanta@gmail.com>
2024-08-10 21:43:56 -0500

    bugfix: horizontal movement fix
    idk how to fix vertical PLEASE HELP JUDGE

--- player.gd
+++ player.gd
@@ -3,39 +3,47 @@ const speed : int = 500
 const jump_power: int = -2000
 const acc : int = 50
 const friction : int = 70 # > than acc
-const gravity : int = 500
+const gravity : int = 140
 const max_jumps : int = 2
-var current_jumps : int = 1
+var current_jumps : int = 0
+
 func _physics_process(delta):
 	var input_dir : Vector2 = input()
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		velocity.y = gravity * delta
 	if input_dir != Vector2.ZERO:
-		accelerate(input_dir)
+		velocity = velocity.move_toward(speed * input_dir, acc) #Accerlate
 		play_animation()
 	else:
-		add_friction()
+		velocity = velocity.move_toward(Vector2.ZERO, friction)
 		play_animation()
-	jump(delta)
-	player_movement()
+	jump()
+	move_and_slide()
 	
 func input() -> Vector2:
 	var input_dir = Vector2.ZERO
-	
 	input_dir.x = Input.get_axis("move_left", "move_right")
 	return input_dir
-func accelerate(direction):
-	velocity = velocity.move_toward(speed * direction, acc)
-func add_friction():
-	velocity = velocity.move_toward(Vector2.ZERO, friction)
-func player_movement():
-	move_and_slide()
 func play_animation():
-	pass
-func jump(delta):
+	if Input.is_action_pressed("jump"):
+		$AnimatedSprite2D.animation = "jump"
+	elif Input.is_action_pressed("duck"):
+		$AnimatedSprite2D.animation = "duck"
+	elif velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+	else:
+		$AnimatedSprite2D.animation = "idle"
+	
+	
+	if velocity.x > 0:
+		$AnimatedSprite2D.flip_h = true
+	else:
+		$AnimatedSprite2D.flip_h = false
+	
+func jump():
 	if Input.is_action_pressed("jump"):
 		if current_jumps < max_jumps:
 			velocity.y = jump_power
 			current_jumps += 1
 		if is_on_floor():
-			current_jumps = 1
+			current_jumps = 0
