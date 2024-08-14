ed5034450211036612d8f7308794cd04c2b028ff
Sonu <bhaarat.mohanta@gmail.com>
2024-08-13 23:14:12 -0500

    feat: Lemon can die!

--- scripts/lemon.gd
+++ scripts/lemon.gd
@@ -1,16 +1,18 @@
 extends CharacterBody2D
-var speed = -250
+var speed : int = -250
+var dead : bool = false
 @onready var ray_cast = $RayCast2D
-@onready var player = "res://scripts/player.gd"
+
 func _ready():
 	$AnimatedSprite2D.play("walk")
 
 func _physics_process(_delta):
-	if ray_cast.is_colliding() and not ray_cast.get_collider().is_in_group("player"):
-		flip()
-	velocity.x = speed 
-	#print(velocity.x)
-	move_and_slide()
+	if not dead:
+		if ray_cast.is_colliding() and not ray_cast.get_collider().is_in_group("player"):
+			flip()
+		velocity.x = speed 
+		#print(velocity.x)
+		move_and_slide()
 	
 
 func flip():
@@ -18,4 +20,15 @@ func flip():
 	$RayCast2D.position.x *= -1
 	$RayCast2D.set_rotation_degrees(180 if $RayCast2D.get_rotation_degrees() == 0 else 0)
 	speed *= -1
-	
+
+
+
+
+func stabbed(body):
+	if body.is_in_group("weapon"):
+		dead = true
+		$AnimatedSprite2D.play("death")
+		$CollisionShape2D.position.y = -6.765
+		$CollisionShape2D.rotation = 90
+		$hurtbox/CollisionShape2D.disabled = true
+		$RayCast2D.enabled = false
